a3df48644cde25df2006c4bdba32742900239232
stacksagar <stacksagar@gmail.com>
2023-03-02 08:52:26 +0600

    binary search ascending & decending implemented

--- Searching/binarySearch.js
+++ Searching/binarySearch.js
@@ -12,19 +12,19 @@ function bSearch(numbers = [], target = 0) {
     if (target === middleValue) {
       return middleIndex;
     } else if (target > middleValue) {
-      isAscending ? (startIndex = middleIndex + 1) : (startIndex = 0);
+      isAscending
+        ? (startIndex = middleIndex + 1)
+        : (endIndex = middleIndex - 1);
     } else {
-      isAscending ?  endIndex = middleIndex - 1 :
+      isAscending
+        ? (endIndex = middleIndex - 1)
+        : (startIndex = middleIndex + 1);
     }
   }
 
   return -1;
 }
 
-console.log(
-  bSearch([10, 20, 30, 40, 50, 60, 70, 80, 90, 99, 110, 120, 130, 140, 150], 10)
-);
+console.log(bSearch([10, 20, 30, 40, 50, 60, 70, 80, 90, 99], 40));
 
-console.log(
-  bSearch([150, 140, 130, 120, 110, 99, 90, 80, 70, 60, 50, 40, 30, 20, 10], 10)
-);
+console.log(bSearch([99, 90, 80, 70, 60, 50, 40, 30, 20, 10], 10));
