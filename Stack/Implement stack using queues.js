abf9fa8c01f1ee6268f003754df69f514cb5cc90
stacksagar <stacksagar@gmail.com>
2023-04-04 00:00:29 +0600

    updated

---
+++ Stack/Implement stack using queues.js
@@ -0,0 +1,23 @@
+class Node {}
+
+class MyQueue {
+  constructor() {}
+
+  queue() {}
+  dequeue() {}
+}
+
+class MyStack {
+  constructor() {
+    this.q1 = new MyStack();
+    this.q2 = new MyStack();
+  }
+
+  push(value) {}
+  pop() {}
+  top() {}
+  empty() {}
+}
+
+const myStack = new MyStack();
+  
