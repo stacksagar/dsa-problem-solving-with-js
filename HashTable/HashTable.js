331632ff6fbce4c24cb32b1e9e71a9f2027d90fa
stacksagar <stacksagar@gmail.com>
2023-04-07 11:10:04 +0600

    updated

--- HashTable/HashTable.js
+++ HashTable/HashTable.js
@@ -52,6 +52,7 @@ table.set({
 console.log(table.table[0]);
 console.log(table.table[1]);
 console.log(table.table[2]);
+
 console.log(table.table[3]);
 console.log(table.table[4]);
 console.log(table.get(10));
