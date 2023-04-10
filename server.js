9872c8eb70337a85fd808f94843f3b0ac750578c
stacksagar <stacksagar@gmail.com>
2023-04-10 22:46:14 +0600

    updated

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from "express";
 import l_list from "./server/routers/linkedlists.js";
 const app = express();
-
 app.get("/health", (_req, res) => {
   res.json({ message: "ok", data: {} });
 });
-
 app.use("/LinkedLists", l_list);
-
 app.listen(1000, () => {
   console.log("Server running");
 });
