50e951e50ec0a30bfafd794168a882a4aafafa56
stacksagar <stacksagar@gmail.com>
2023-04-15 00:01:24 +0600

    updated

---
+++ Recursion/509. Fibonacci Number.ts
@@ -0,0 +1,14 @@
+let count = 0;
+function fib(n: number): any {
+  count++;
+  if (n <= 1) {
+    console.log(`(count is ${count})`);
+    console.log(`(n is ${n})`);
+    return n;
+  }
+
+  const val = fib(n - 1) + fib(n - 2);
+  return val;
+}
+
+console.log(fib(4));
